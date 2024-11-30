e93de788e042fbc9495894293a69447c3765d565
Anton Petukhov <petuhova805@gmail.com>
2024-11-30 17:16:57 +0300

    feat: Конфигурирование сервиса через API, изменил формат маршрутов(теперь запросы отправляются на один endpoint)

--- scripts/SQL/install.py
+++ scripts/SQL/install.py
@@ -2,43 +2,6 @@ import psycopg2
 from colorama import Fore, Back, Style, init
 import configparser
 
-print('- Reading database connection config')
-
-config = configparser.ConfigParser()
-config.read('./config.ini')
-
-host = config['Connection']['Host']
-port = config['Connection']['Port']
-database = config['Connection']['Database']
-user = config['Connection']['User']
-password = config['Connection']['Password']
-
-print('-' * 50)
-
-print(f'DATABASE CONNECTION CONFIG')
-print(f'* Host = {host}')
-print(f'* Port = {port}')
-print(f'* Database = {database}')
-print(f'* User = {user}')
-print(f'* Password = {password}')
-
-print('-' * 50)
-
-print('- Try to connect PostgreSQL')
-connection = None
-try:
-    connection = psycopg2.connect(
-        host=host,
-        port=port,
-        database=database,
-        user=user,
-        password=password
-    )
-    print(Fore.GREEN + 'Connection established' + Style.RESET_ALL)
-except:
-    print(Fore.RED + "Can't to connect PostgreSQL" + Style.RESET_ALL)
-    exit(1)
-
 print('- Reading SQL script')
 
 script = ''
@@ -46,8 +9,45 @@ with open('./schema.sql') as file:
     script = file.read()
 
 print('- SQL script is read')
-print('- Executing...')
 
-with connection.cursor() as cursor:
-    cursor.execute(script)
-    print(Fore.GREEN + 'Done' + Style.RESET_ALL)
+print('- Reading database connection config')
+
+config = configparser.ConfigParser()
+config.read('./config.ini')
+
+for section in config.sections():
+    print(f'- Install for section {section}:')
+    host = config[section]['Host']
+    port = config[section]['Port']
+    database = config[section]['Database']
+    user = config[section]['User']
+    password = config[section]['Password']
+    print(f'* Host = {host}')
+    print(f'* Port = {port}')
+    print(f'* Database = {database}')
+    print(f'* User = {user}')
+    print(f'* Password = {password}')
+
+    print('- Try to connect')
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=user,
+            password=password
+        )
+        print(Fore.GREEN + 'Connection established' + Style.RESET_ALL)
+    except:
+        print(Fore.RED + f"Can't to connect {section} section" + Style.RESET_ALL)
+        continue
+
+    print('- Executing...')
+    
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(script)
+            print(Fore.GREEN + 'Done' + Style.RESET_ALL)
+    except Exception as e:
+        print(Fore.RED + f"Can't to execute script for {section} section. {str(e)}")
---
+++ src/TPlatform.TelegramService/TPlatform.TelegramService.Services/Implementation/ConfigurationService.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TPlatform.TelegramService.Dto;
+using TPlatform.TelegramService.Services.Interfaces;
+
+namespace TPlatform.TelegramService.Services.Implementation
+{
+	public class ConfigurationService : IConfigurationService
+	{
+        private readonly IApplicationContext _applicationContext;
+
+		public ConfigurationService(IApplicationContext applicationContext)
+		{
+            _applicationContext = applicationContext;
+		}
+
+        public async Task<ServiceResult<AllowedChannel>> AddAllowedChannelAsync(long channelId)
+        {
+            var existingChannel = await _applicationContext.AllowedChannels
+                .FirstOrDefaultAsync(channel => channel.ChannelId == channelId);
+
+            if (existingChannel != null)
+            {
+                return new ServiceResult<AllowedChannel>
+                {
+                    ResultType = ServiceResultType.BadRequest,
+                    ErrorMessage = $"Channel with id {channelId} already exists"
+                };
+            }
+
+            var allowedChannel = new AllowedChannel
+            {
+                ChannelId = channelId
+            };
+
+            await _applicationContext.AllowedChannels.AddAsync(allowedChannel);
+            await _applicationContext.SaveChangesAsync();
+
+            return new ServiceResult<AllowedChannel>
+            {
+                Result = allowedChannel,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+
+        public async Task<ServiceResult<AllowedUser>> AddAllowedUserAsync(long userId)
+        {
+            var existingUser = await _applicationContext.AllowedUsers
+                .FirstOrDefaultAsync(user => user.UserId == userId);
+
+            if(existingUser != null)
+            {
+                return new ServiceResult<AllowedUser>
+                {
+                    ResultType = ServiceResultType.BadRequest,
+                    ErrorMessage = $"User with id {userId} already exists"
+                };
+            }
+
+            var allowedUser = new AllowedUser
+            {
+                UserId = userId
+            };
+
+            await _applicationContext.AllowedUsers.AddAsync(allowedUser);
+            await _applicationContext.SaveChangesAsync();
+
+            return new ServiceResult<AllowedUser>
+            {
+                Result = allowedUser,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+
+        public async Task<ServiceResult<bool>> RemoveAllowedUserByIdAsync(int id)
+        {
+            var existingUser = await _applicationContext.AllowedUsers
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if(existingUser == null)
+            {
+                return new ServiceResult<bool>
+                {
+                    Result = false,
+                    ResultType = ServiceResultType.NotFound,
+                    ErrorMessage = $"User with id {id} not exists"
+                };
+            }
+
+            _applicationContext.AllowedUsers.Remove(existingUser);
+            await _applicationContext.SaveChangesAsync();
+
+            return new ServiceResult<bool>
+            {
+                Result = true,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+
+        public async Task<ServiceResult<bool>> RemoveAllowedChannelByIdAsync(int id)
+        {
+            var existingChannel = await _applicationContext.AllowedChannels
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if(existingChannel == null)
+            {
+                return new ServiceResult<bool>
+                {
+                    Result = false,
+                    ResultType = ServiceResultType.NotFound,
+                    ErrorMessage = $"Channel with id {id} not exists"
+                };
+            }
+
+            _applicationContext.AllowedChannels.Remove(existingChannel);
+            await _applicationContext.SaveChangesAsync();
+
+            return new ServiceResult<bool>
+            {
+                Result = true,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+
+        public async Task<ServiceResult<List<AllowedChannel>>> GetAllAllowedChannelsAsync()
+        {
+            var allowedChannels = await _applicationContext.AllowedChannels.ToListAsync();
+            return new ServiceResult<List<AllowedChannel>>
+            {
+                Result = allowedChannels,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+
+        public async Task<ServiceResult<List<AllowedUser>>> GetAllAllowedUsersAsync()
+        {
+            var allowedUsers = await _applicationContext.AllowedUsers.ToListAsync();
+            return new ServiceResult<List<AllowedUser>>
+            {
+                Result = allowedUsers,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+
+        public async Task<ServiceResult<AllowedChannel>> GetAllowedChannelByIdAsync(int id)
+        {
+            var allowedChannel = await _applicationContext.AllowedChannels
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if(allowedChannel == null)
+            {
+                return new ServiceResult<AllowedChannel>
+                {
+                    ResultType = ServiceResultType.NotFound,
+                    ErrorMessage = $"Allowed channel with id {id} not exists"
+                };
+            }
+
+            return new ServiceResult<AllowedChannel>
+            {
+                Result = allowedChannel,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+
+        public async Task<ServiceResult<AllowedUser>> GetAllowedUserByIdAsync(int id)
+        {
+            var allowedUser = await _applicationContext.AllowedUsers
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if(allowedUser == null)
+            {
+                return new ServiceResult<AllowedUser>
+                {
+                    ResultType = ServiceResultType.NotFound,
+                    ErrorMessage = $"Allowed user with id {id} not exists"
+                };
+            }
+
+            return new ServiceResult<AllowedUser>
+            {
+                Result = allowedUser,
+                ResultType = ServiceResultType.Ok
+            };
+        }
+    }
+}
+
